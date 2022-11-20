3d9783a071f870e2d6ab085a85280fce55f97d42
shin-f0318 <sf.0318ss@gmail.com>
2022-11-20 16:30:27 +0900

    commit

--- resources/views/practice/edit.blade.php
+++ resources/views/practice/edit.blade.php
@@ -5,7 +5,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        @vite(['resources/js/app.js'])
+       
         <title>お問合せ内容編集</title>    
     </head>
 
